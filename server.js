941166be66d911858069b555610f8e10d5c1bbf9
Jefferson Ribeiro <jeffersoft1@gmail.com>
2020-04-16 22:22:55 +0100

    Waves Keeper Auth integrated

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.static(__dirname + '/dist'));
 app.get('/', (req, res) => {
 	res.sendFile('index.html', { root : __dirname});
 });
